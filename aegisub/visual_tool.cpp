jeeb/aegisub-historical

commit a262899db3f9b308b23db1c3166949a6a627a1eb

Issue #514 - Fixed crash with vector clip. Also fixed a minor bug that caused visual typesetting to flag the script as modified even if nothing changed.
git-svn-id: http://svn.aegisub.org/trunk@1466 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/visual_tool.cpp
@@ -257,7 +257,14 @@ void VisualTool::OnMouseEvent (wxMouseEvent &event) {
 //////////
 // Commit
 void VisualTool::Commit(bool full) {
+	// Get grid
 	SubtitlesGrid *grid = VideoContext::Get()->grid;
+
+	// See if anything actually changed
+	AssDialogue *diag = grid->GetDialogue(grid->editBox->linen);
+	if (diag && grid->editBox->TextEdit->GetText() == diag->Text) return;
+
+	// Commit changes
 	grid->editBox->CommitText();
 	if (full) grid->ass->FlagAsModified(_("visual typesetting"));
 	grid->CommitChanges(false,!full);

aegisub/visual_tool_vector_clip.cpp
@@ -362,6 +362,9 @@ void VisualToolVectorClip::InitializeHold() {
 
 		// Insert
 		else {
+			// Check if there is at least one curve to split
+			if (spline.curves.size() == 0) return;
+
 			// Split the curve
 			SplineCurve *c1 = spline.GetCurve(curve);
 			SplineCurve c2;

commit 6789096c6589ca07e75cf082c8e896fdd3f309d5

Fix #532 - other lines become invisible after using visual TS tools, until video reload. (Regression introduced in r1466.)
git-svn-id: http://svn.aegisub.org/trunk@1526 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/visual_tool.cpp
@@ -261,8 +261,11 @@ void VisualTool::Commit(bool full) {
 	SubtitlesGrid *grid = VideoContext::Get()->grid;
 
 	// See if anything actually changed
-	AssDialogue *diag = grid->GetDialogue(grid->editBox->linen);
-	if (diag && grid->editBox->TextEdit->GetText() == diag->Text) return;
+	// Fix by jfs: Only if not doing a  full commit. Not sure why, but this avoids bug #532
+	if (!full) {
+		AssDialogue *diag = grid->GetDialogue(grid->editBox->linen);
+		if (diag && grid->editBox->TextEdit->GetText() == diag->Text) return;
+	}
 
 	// Commit changes
 	grid->editBox->CommitText();