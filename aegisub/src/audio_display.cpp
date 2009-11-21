jeeb/aegisub-historical

commit c1a9bf9dffadc7a2dffcdda71a5565509da0a6a5

Merge revisions r3675, r3676 and r3688 from trunk (detached video fixes).
git-svn-id: http://svn.aegisub.org/branches/aegisub_2.1.8@3797 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/audio_display.cpp
@@ -1348,7 +1348,7 @@ END_EVENT_TABLE()
 /////////
 // Paint
 void AudioDisplay::OnPaint(wxPaintEvent& event) {
-	if (w == 0 || h == 0) return;
+	if (w == 0 || h == 0 || !origImage) return;
 	DoUpdateImage();
 
 	wxPaintDC dc(this);

aegisub/src/dialog_detached_video.cpp
@@ -34,8 +34,6 @@
 //
 
 
-///////////
-// Headers
 #include "config.h"
 
 #include <wx/wxprec.h>
@@ -53,13 +51,12 @@
 #undef min
 #undef max
 
-///////////////
-// Constructor
+/// @brief Constructor
+/// @param par FrameMain this was spawned from
+/// @param initialDisplaySize Initial size of the window
 DialogDetachedVideo::DialogDetachedVideo(FrameMain *par, const wxSize &initialDisplaySize)
-//: wxFrame(par,-1,_("Detached Video"))
 : wxDialog(par,-1,_T("Detached Video"),wxDefaultPosition,wxSize(400,300),wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER | wxMAXIMIZE_BOX | wxMINIMIZE_BOX | wxWANTS_CHARS)
 {
-	// Set parent
 	parent = par;
 
 	// Set up window
@@ -125,58 +122,42 @@ DialogDetachedVideo::DialogDetachedVideo(FrameMain *par, const wxSize &initialDi
 	}
 
 	// Update
-	parent->SetDisplayMode(0,-1);
+	parent->SetDisplayMode(0, -1);
 	GetPosition(&x, &y);
 	Options.SetInt(_T("Detached video last x"), x);
 	Options.SetInt(_T("Detached video last y"), y);
 	Options.SetBool(_T("Detached video"),true);
 	Options.Save();
+
+	// Copy the main accelerator table to this dialog
+	wxAcceleratorTable *table = par->GetAcceleratorTable();
+	SetAcceleratorTable(*table);
 }
 
-
-/////////////
-// Destructor
+/// @brief Destructor
 DialogDetachedVideo::~DialogDetachedVideo() {
 	Options.SetBool(_T("Detached video maximized"),IsMaximized());
 	Options.Save();
 }
 
-
-///////////////
 // Event table
 BEGIN_EVENT_TABLE(DialogDetachedVideo,wxDialog)
-	EVT_KEY_DOWN(DialogDetachedVideo::OnKey)
 	EVT_CLOSE(DialogDetachedVideo::OnClose)
 	EVT_MOVE(DialogDetachedVideo::OnMove)
 END_EVENT_TABLE()
 
-
-////////////
-// Key down
-void DialogDetachedVideo::OnKey(wxKeyEvent &event) {
-	// Send to parent... except that it doesn't work
-	event.Skip();
-#if wxCHECK_VERSION(2,9,0)
-	GetParent()->GetEventHandler()->ProcessEvent(event);
-#else
-	GetParent()->AddPendingEvent(event);
-#endif
-}
-
-
-////////////////
-// Close window
-void DialogDetachedVideo::OnClose(wxCloseEvent &event) {
+/// @brief Close window
+/// @param event UNUSED
+void DialogDetachedVideo::OnClose(wxCloseEvent &WXUNUSED(event)) {
 	FrameMain *par = parent;
 	Options.SetBool(_T("Detached video"),false);
 	Destroy();
 	par->detachedVideo = NULL;
-	par->SetDisplayMode(-1,-1);
+	par->SetDisplayMode(1,-1);
 }
 
-
-///////////////
-// Move window
+/// @brief Move window 
+/// @param event 
 void DialogDetachedVideo::OnMove(wxMoveEvent &event) {
 	wxPoint pos = event.GetPosition();
 	Options.SetInt(_T("Detached video last x"),pos.x);

aegisub/src/dialog_detached_video.h
@@ -33,27 +33,22 @@
 // Contact: mailto:zeratul@cellosoft.com
 //
 
-
-#pragma once
-
-
 #include <wx/dialog.h>
 
-
-//////////////
-// Prototypes
 class VideoBox;
 class FrameMain;
 
 
-//////////////////////////////
-// Detached video frame class
+/// DOCME
+/// @class DialogDetachedVideo
+/// @brief DOCME
+///
+/// DOCME
 class DialogDetachedVideo : public wxDialog {
 private:
 	VideoBox *videoBox;
 	FrameMain *parent;
 
-	void OnKey(wxKeyEvent &event);
 	void OnClose(wxCloseEvent &event);
 	void OnMove(wxMoveEvent &event);
 

aegisub/src/frame_main.cpp
@@ -567,6 +567,7 @@ void FrameMain::InitContents() {
 	StartupLog(_T("Create video box"));
 	videoBox = new VideoBox(Panel, false);
 	TopSizer->Add(videoBox,0,wxEXPAND,0);
+	TopSizer->Show(videoBox, false, true);
 	videoBox->videoDisplay->zoomBox = ZoomBox;
 
 	// Subtitles area
@@ -594,6 +595,7 @@ void FrameMain::InitContents() {
 	StartupLog(_T("Arrange controls in sizers"));
 	ToolSizer = new wxBoxSizer(wxVERTICAL);
 	ToolSizer->Add(audioBox,0,wxEXPAND | wxBOTTOM,5);
+	ToolSizer->Show(audioBox, false, true);
 	ToolSizer->Add(EditBox,1,wxEXPAND,5);
 	TopSizer->Add(ToolSizer,1,wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM,5);
 
@@ -607,8 +609,6 @@ void FrameMain::InitContents() {
 	//SetSizer(MainSizer);
 
 	// Set display
-	StartupLog(_T("Set display mode"));
-	SetDisplayMode(0,0);
 	StartupLog(_T("Perform layout"));
 	Layout();
 	StartupLog(_T("Set focus to edting box"));
@@ -641,9 +641,9 @@ void FrameMain::UpdateToolbar() {
 	// Update
 	wxToolBar* toolbar = GetToolBar();
 	toolbar->FindById(Menu_Video_JumpTo)->Enable(isVideo);
-	toolbar->FindById(Menu_Video_Zoom_In)->Enable(isVideo);
-	toolbar->FindById(Menu_Video_Zoom_Out)->Enable(isVideo);
-	ZoomBox->Enable(isVideo);
+	toolbar->FindById(Menu_Video_Zoom_In)->Enable(isVideo && !detachedVideo);
+	toolbar->FindById(Menu_Video_Zoom_Out)->Enable(isVideo && !detachedVideo);
+	ZoomBox->Enable(isVideo && !detachedVideo);
 	toolbar->FindById(Menu_Subs_Snap_Start_To_Video)->Enable(isVideo && selRows > 0);
 	toolbar->FindById(Menu_Subs_Snap_End_To_Video)->Enable(isVideo && selRows > 0);
 	toolbar->FindById(Menu_Subs_Snap_Video_To_Start)->Enable(isVideo && selRows == 1);
@@ -810,24 +810,26 @@ int FrameMain::TryToCloseSubs(bool enableCancel) {
 }
 
 
-////////////////////
-// Set display mode
-void FrameMain::SetDisplayMode(int _showVid,int _showAudio) {
-	// Shown?
-	static bool firstRun = true;
-	if (!IsShownOnScreen() && !firstRun) return;
-	firstRun = false;
 
-	// Automatic
-	if (_showVid == -1) _showVid = (VideoContext::Get()->IsLoaded() && !detachedVideo) ? 1 : 0;
-	else if (_showVid == -2) _showVid = showVideo?1:0;
-	if (_showAudio == -1) _showAudio = audioBox->loaded ? 1 : 0;
-	else if (_showAudio == -2) _showAudio = showAudio?1:0;
+/// @brief Set the video and audio display visibilty
+/// @param video -1: leave unchanged; 0: hide; 1: show
+/// @param audio -1: leave unchanged; 0: hide; 1: show
+void FrameMain::SetDisplayMode(int video, int audio) {
+	if (!IsShownOnScreen()) return;
+
+	bool sv = false, sa = false;
+
+	if (video == -1) sv = showVideo;
+	else if (video)  sv = VideoContext::Get()->IsLoaded() && !detachedVideo;
+
+	if (audio == -1) sa = showAudio;
+	else if (audio)  sa = audioBox->loaded;
 
 	// See if anything changed
-	if (_showVid == (showVideo?1:0) && _showAudio == (showAudio?1:0)) return;
-	showAudio = _showAudio == 1;
-	showVideo = _showVid == 1;
+	if (sv == showVideo && sa == showAudio) return;
+
+	showVideo = sv;
+	showAudio = sa;
 
 	// Stop
 	Freeze();
@@ -842,7 +844,6 @@ void FrameMain::SetDisplayMode(int _showVid,int _showAudio) {
 	EditBox->SetSplitLineMode();
 	MainSizer->CalcMin();
 	MainSizer->RecalcSizes();
-	//videoBox->VideoSizer->Layout();
 	MainSizer->Layout();
 	Layout();
 	Show(true);
@@ -1009,7 +1010,7 @@ void FrameMain::SynchronizeProject(bool fromSubs) {
 		}
 
 		// Display
-		SetDisplayMode(-1,-1);
+		SetDisplayMode(1,1);
 	}
 
 	// Store data on subs
@@ -1096,9 +1097,7 @@ void FrameMain::LoadVideo(wxString file,bool autoload) {
 				VFR_Output.Unload();
 			}
 		}
-		SetDisplayMode(1,-1);
 		VideoContext::Get()->SetVideo(file);
-		SetDisplayMode(0,-1);
 	}
 	catch (const wchar_t *error) {
 		wxString err(error);
@@ -1136,7 +1135,7 @@ void FrameMain::LoadVideo(wxString file,bool autoload) {
 	}
 
 	SubsBox->CommitChanges(true);
-	SetDisplayMode(-1,-1);
+	SetDisplayMode(1,-1);
 	EditBox->UpdateFrameTiming();
 
 	DetachVideo(VideoContext::Get()->IsLoaded() && Options.AsBool(_T("Detached Video")));
@@ -1151,7 +1150,7 @@ void FrameMain::LoadAudio(wxString filename,bool FromVideo) {
 	VideoContext::Get()->Stop();
 	try {
 		audioBox->SetFile(filename,FromVideo);
-		SetDisplayMode(-1,-1);
+		SetDisplayMode(-1,1);
 	}
 	catch (const wchar_t *error) {
 		wxString err(error);
@@ -1215,8 +1214,9 @@ void FrameMain::OpenHelp(wxString page) {
 }
 
 
-///////////////////////
-// Detach video window
+
+/// @brief Detach video window 
+/// @param detach 
 void FrameMain::DetachVideo(bool detach) {
 	if (detach) {
 		if (!detachedVideo) {
@@ -1224,13 +1224,12 @@ void FrameMain::DetachVideo(bool detach) {
 			detachedVideo->Show();
 		}
 	}
-	else {
-		if (detachedVideo) {
-			detachedVideo->Destroy();
-			SetDisplayMode(-1,-1);
-			detachedVideo = NULL;
-		}
+	else if (detachedVideo) {
+		detachedVideo->Destroy();
+		detachedVideo = NULL;
+		SetDisplayMode(1,-1);
 	}
+	UpdateToolbar();
 }
 
 

aegisub/src/frame_main_events.cpp
@@ -286,7 +286,7 @@ void FrameMain::OnMenuOpen (wxMenuEvent &event) {
 	else if (curMenu == viewMenu) {
 		// Flags
 		bool aud = audioBox->audioDisplay->loaded;
-		bool vid = VideoContext::Get()->IsLoaded();
+		bool vid = VideoContext::Get()->IsLoaded() && !detachedVideo;
 
 		// Set states
 		MenuBar->Enable(Menu_View_Audio,aud);
@@ -303,23 +303,24 @@ void FrameMain::OnMenuOpen (wxMenuEvent &event) {
 	// Video menu
 	else if (curMenu == videoMenu) {
 		bool state = VideoContext::Get()->IsLoaded();
+		bool attached = state && !detachedVideo;
 
 		// Set states
 		MenuBar->Enable(Menu_Video_JumpTo,state);
 		MenuBar->Enable(Menu_Subs_Snap_Video_To_Start,state);
 		MenuBar->Enable(Menu_Subs_Snap_Video_To_End,state);
-		MenuBar->Enable(Menu_View_Zoom,state);
-		MenuBar->Enable(Menu_View_Zoom_50,state);
-		MenuBar->Enable(Menu_View_Zoom_100,state);
-		MenuBar->Enable(Menu_View_Zoom_200,state);
+		MenuBar->Enable(Menu_View_Zoom,attached);
+		MenuBar->Enable(Menu_View_Zoom_50,attached);
+		MenuBar->Enable(Menu_View_Zoom_100,attached);
+		MenuBar->Enable(Menu_View_Zoom_200,attached);
 		MenuBar->Enable(Menu_File_Close_Video,state);
-		MenuBar->Enable(Menu_Video_AR,state);
-		MenuBar->Enable(Menu_Video_AR_Default,state);
-		MenuBar->Enable(Menu_Video_AR_Full,state);
-		MenuBar->Enable(Menu_Video_AR_Wide,state);
-		MenuBar->Enable(Menu_Video_AR_235,state);
-		MenuBar->Enable(Menu_Video_AR_Custom,state);
-		MenuBar->Enable(Menu_Video_Detach,state && !detachedVideo);
+		MenuBar->Enable(Menu_Video_AR,attached);
+		MenuBar->Enable(Menu_Video_AR_Default,attached);
+		MenuBar->Enable(Menu_Video_AR_Full,attached);
+		MenuBar->Enable(Menu_Video_AR_Wide,attached);
+		MenuBar->Enable(Menu_Video_AR_235,attached);
+		MenuBar->Enable(Menu_Video_AR_Custom,attached);
+		MenuBar->Enable(Menu_Video_Detach,state);
 		MenuBar->Enable(Menu_File_Save_VFR,VFR_Output.GetFrameRateType() == VFR);
 		MenuBar->Enable(Menu_File_Close_VFR,VFR_Output.GetFrameRateType() == VFR);
 		MenuBar->Enable(Menu_Video_Close_Keyframes,VideoContext::Get()->OverKeyFramesLoaded());
@@ -899,7 +900,7 @@ void FrameMain::OnSetZoom(wxCommandEvent &event) {
 ////////////////
 // Detach video
 void FrameMain::OnDetachVideo(wxCommandEvent &event) {
-	DetachVideo();
+	DetachVideo(!detachedVideo);
 }
 
 
@@ -1320,7 +1321,7 @@ void FrameMain::OnReplace(wxCommandEvent &event) {
 void FrameMain::OnSetARDefault (wxCommandEvent &event) {
 	VideoContext::Get()->Stop();
 	VideoContext::Get()->SetAspectRatio(0);
-	SetDisplayMode(-1,-2);
+	SetDisplayMode(1,-1);
 }
 
 
@@ -1329,7 +1330,7 @@ void FrameMain::OnSetARDefault (wxCommandEvent &event) {
 void FrameMain::OnSetARFull (wxCommandEvent &event) {
 	VideoContext::Get()->Stop();
 	VideoContext::Get()->SetAspectRatio(1);
-	SetDisplayMode(-1,-2);
+	SetDisplayMode(1,-1);
 }
 
 
@@ -1338,7 +1339,7 @@ void FrameMain::OnSetARFull (wxCommandEvent &event) {
 void FrameMain::OnSetARWide (wxCommandEvent &event) {
 	VideoContext::Get()->Stop();
 	VideoContext::Get()->SetAspectRatio(2);
-	SetDisplayMode(-1,-2);
+	SetDisplayMode(1,-1);
 }
 
 
@@ -1347,7 +1348,7 @@ void FrameMain::OnSetARWide (wxCommandEvent &event) {
 void FrameMain::OnSetAR235 (wxCommandEvent &event) {
 	VideoContext::Get()->Stop();
 	VideoContext::Get()->SetAspectRatio(3);
-	SetDisplayMode(-1,-2);
+	SetDisplayMode(1,-1);
 }
 
 
@@ -1397,7 +1398,7 @@ void FrameMain::OnSetARCustom (wxCommandEvent &event) {
 	// Set value
 	else {
 		VideoContext::Get()->SetAspectRatio(4,numval);
-		SetDisplayMode(-1,-1);
+		SetDisplayMode(1,-1);
 	}
 }
 
@@ -1707,7 +1708,6 @@ void FrameMain::OnPickAssociations(wxCommandEvent &event) {
 /////////////////
 // View standard
 void FrameMain::OnViewStandard (wxCommandEvent &event) {
-	if (!audioBox->audioDisplay->loaded || !VideoContext::Get()->IsLoaded()) return;
 	SetDisplayMode(1,1);
 }
 
@@ -1715,7 +1715,6 @@ void FrameMain::OnViewStandard (wxCommandEvent &event) {
 //////////////
 // View video
 void FrameMain::OnViewVideo (wxCommandEvent &event) {
-	if (!VideoContext::Get()->IsLoaded()) return;
 	SetDisplayMode(1,0);
 }
 
@@ -1723,7 +1722,6 @@ void FrameMain::OnViewVideo (wxCommandEvent &event) {
 //////////////
 // View audio
 void FrameMain::OnViewAudio (wxCommandEvent &event) {
-	if (!audioBox->audioDisplay->loaded) return;
 	SetDisplayMode(0,1);
 }
 

commit 81ea9a395ffcfb580a5865b0de51a8f0c1ad080a

Fix regression introduced by r3797. The OnPaint handler would be skipped if origImage was 0, but the only thing that could initialise origImage to non-0 was DoUpdateImage called on the next line, thus never called.
git-svn-id: http://svn.aegisub.org/branches/aegisub_2.1.8@3812 93549f3f-7f0a-0410-a4b3-e966c9c94f04

aegisub/src/audio_display.cpp
@@ -1348,11 +1348,11 @@ END_EVENT_TABLE()
 /////////
 // Paint
 void AudioDisplay::OnPaint(wxPaintEvent& event) {
-	if (w == 0 || h == 0 || !origImage) return;
+	if (w == 0 || h == 0) return;
 	DoUpdateImage();
 
 	wxPaintDC dc(this);
-	dc.DrawBitmap(*origImage,0,0);
+	if (origImage) dc.DrawBitmap(*origImage,0,0);
 }
 
 